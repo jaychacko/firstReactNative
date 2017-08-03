e1ee9505335772fe69bcab2d7862e458e65a09c9
jaychacko <chackojayui@gmail.com>
2017-08-02 21:54:28 -0500

    style

--- ProjectName/src/components/AlbumDetail.js
+++ ProjectName/src/components/AlbumDetail.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import { Text } from 'react-native';
 import Card from './card'
-
+import CardSection from './cardSection'
 const AlbumDetail = (props) => (
         <Card>
+            <CardSection>
             <Text>{props.albumData.title}</Text>
+            </CardSection>
         </Card>
 
     );
